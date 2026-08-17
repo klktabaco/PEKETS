5dc18a7f43271941059e91e36bcdf5435c6bf8a9
klktabaco <guillemmg118@gmail.com>
2026-08-17 15:19:44 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.7.1/firebase-app-compat.js"
+);
+
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.7.1/firebase-messaging-compat.js"
+);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDIT5zXfsGYNUxepsGUVAzD9bhTemeoq1A",
+  authDomain: "pekets-4f821.firebaseapp.com",
+  projectId: "pekets-4f821",
+  storageBucket: "pekets-4f821.firebasestorage.app",
+  messagingSenderId: "949543833442",
+  appId: "1:949543833442:web:973bf89f058020c6e8e63b"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+
+  console.log("Notificación recibida:", payload);
+
+  const title = payload.notification?.title || "PEKETS ❤️";
+
+  const options = {
+    body: payload.notification?.body || "Tienes una nueva solicitud 💕",
+    icon: "./icon.png"
+  };
+
+  self.registration.showNotification(title, options);
+
+});
